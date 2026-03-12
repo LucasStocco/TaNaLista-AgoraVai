5260e7052f2ee2107e1158c4bc97ac09c7a3cc43
wlb-f <wlb0980@gmail.com>
2026-03-12 19:27:36 -0300

    Update and rename Financeiro.java to back-end/src/main/java/br/com/prototype/listacompras/model/Financeiro.java

---
+++ FinanceiroService.java
@@ -0,0 +1,21 @@
+package br.com.prototype.listacompras.service;
+ 
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import br.com.prototype.listacompras.repository.ItemRepository;
+
+@Service
+public class FinanceiroService {
+    
+    @Autowired
+    private ItemRepository itemRepository;
+    
+    public Map<String, Object> gerarRelatorio() {
+        Map<String, Object> relatorio = new HashMap<>();
+        relatorio.put("porLista", itemRepository.consultarGastoPorLista());
+        relatorio.put("porCategoria", itemRepository.consultarGastoPorCategoria());
+        return relatorio;
+    }
+}
